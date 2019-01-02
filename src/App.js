542608191355d09c32438fefb2b7afe8a98632eb
denishvachhani <denishvachhani@Air.lan>
2019-01-02 08:30:40 +0100

    Added loginForm

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
 import NavBar from "./components/navBar";
-import "./App.css";
 import MovieForm from "./components/movieForm";
+import LoginForm from "./components/loginForm";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -16,6 +17,7 @@ class App extends Component {
         <main className="container" style={{ marginTop: "30px" }}>
           <Switch>
             <Route path="/movies/:id" component={MovieForm} />
+            <Route path="/login" component={LoginForm} />
             <Route path="/movies" component={Movies} />
             <Route path="/customers" component={Customers} />
             <Route path="/rentals" component={Rentals} />
